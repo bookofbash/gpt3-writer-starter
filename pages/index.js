0713014cc964944943baf8727543a525f37a8c00
Bashir Harrell <bashir@Bashirs-MacBook-Air.local>
2022-12-09 10:54:03 -0500

    Updated some of the css and html content

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,15 @@ const Home = () => {
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>ConversationDesign.ai| Helper</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Conversation Design.ai <span>Helper</span></h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Let's try that in a different voice</h2>
           </div>
         </div>
       </div>
