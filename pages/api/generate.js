b13e6fecb572af407f7d26b374ab766f4843a02f
Bashir Harrell <harrell.bashir@yahoo.com>
2023-01-24 12:51:55 -0500

    Updates to page

---
+++ pages/api/generate.js
@@ -0,0 +1,24 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = "Write the following sentence in  5 various tones of voice and style. Use proper grammar. Create a new line before each example.";
+const generateAction = async (req, res) => {
+   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.4,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import {useState} from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+  
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+  
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+  
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  }
   return (
     <div className="root">
       <Head>
@@ -11,24 +38,41 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Conversation Design.ai <span>Helper</span></h1>
+            <h1>Conversation Design.ai <span>WordWeave</span></h1>
           </div>
           <div className="header-subtitle">
-            <h2>Let's try that in a different voice</h2>
+            <h2>Make Your Conversations More Dynamic. Type a sentence and get creative variations.</h2>
           </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+        <div className="prompt-container">
+          <textarea 
+          placeholder="start typing here" 
+          className="prompt-box"
+          value={userInput}
+          onChange={onUserChangedText} />
+          <div className= "prompt-buttons">
+            <a 
+            className={isGenerating ? 'generate-button loading' : 'generate-button'}  
+            onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+              {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
           </div>
-        </a>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
